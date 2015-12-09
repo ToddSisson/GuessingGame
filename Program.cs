f1b4a9af4acc8ae08ae36039432d63f463283342
Todd Sisson <todd.a.sisson@gmail.com>
2015-12-09 10:57:33 -0600

    Added initial files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ranNumberGenerator = new Random();
+            int randomNumber = ranNumberGenerator.Next(1, 11);
+            Console.Write("Guess a number: ");
+            int selectedNumber = Convert.ToInt32(Console.ReadLine());
+            if (selectedNumber < randomNumber)
+            {
+                Console.WriteLine(randomNumber);
+                Console.WriteLine("Your guess was too low by {0} places", randomNumber - selectedNumber);
+            }
+            else if (selectedNumber > randomNumber)
+            {
+                Console.WriteLine(randomNumber);
+                Console.WriteLine("Your guess was too high by {0} places", selectedNumber - randomNumber);
+            }
+            else if (selectedNumber == randomNumber)
+            {
+                Console.WriteLine(randomNumber);
+                Console.WriteLine("Your guess was correct");
+            }
+}
+    }
+}
